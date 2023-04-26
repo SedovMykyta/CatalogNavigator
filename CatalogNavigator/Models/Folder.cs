0537097f3e1a87b7f12edf242a870d6840e72d42
Mykyta <91759478+SedovMykyta@users.noreply.github.com>
2023-04-26 17:19:08 +0300

    Created infrastructures #1
    feat - Created folder model
    feat - Created db context;
    feat - Created first migrations.

---
+++ CatalogNavigator/Models/Folder.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CatalogNavigator.Models;
+
+public class Folder
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Path { get; set; }
+    public DateTime? CreatedDate { get; set; }
+    public DateTime? ModifiedDate { get; set; }
+    
+    // [ForeignKey("Parent")]
+    public int? ParentId { get; set; }
+    public virtual Folder Parent { get; set; }
+}
