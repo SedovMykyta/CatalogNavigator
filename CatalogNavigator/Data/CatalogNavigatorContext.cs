730534b285b8f8e40fee9d065fd1b9d20ee7c803
Mykyta <91759478+SedovMykyta@users.noreply.github.com>
2023-04-29 18:04:35 +0300

    Prerelease 0.1.0 - Merged develop into master #5
    Created infrastructures;
    Created folder controller;
    Created views;
    Created css styles for project.

---
+++ CatalogNavigator/Data/CatalogNavigatorContext.cs
@@ -0,0 +1,13 @@
+﻿using CatalogNavigator.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatalogNavigator.Data;
+
+public class CatalogNavigatorContext : DbContext
+{
+    public CatalogNavigatorContext(DbContextOptions<CatalogNavigatorContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Folder> Folders { get; set; }
+}
---
+++ CatalogNavigator/Models/Folder.cs
@@ -0,0 +1,15 @@
+﻿namespace CatalogNavigator.Models;
+
+public class Folder
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Path { get; set; }
+    public DateTime? CreatedDate { get; set; }
+    public DateTime? ModifiedDate { get; set; }
+    
+    public int? ParentId { get; set; }
+    public virtual Folder? Parent { get; set; }
+    
+    public List<Folder> Children { get; set; } = new();
+}
